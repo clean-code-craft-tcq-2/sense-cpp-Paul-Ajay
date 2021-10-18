060d9675952023ce7e7e96099cfddfb2b3e427c7
Paul.Ajay-p <Ajay.Paul@in.bosch.com>
2021-10-18 22:22:49 +0530

    Added declarations for classes

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,15 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+Stats Statistics::ComputeStatistics(const std::vector<float>& data) {
+    Stats data_stats;
+    if (data.empty()) {
+        data_stats.average = NAN;
+        data_stats.max = NAN;
+        data_stats.min = NAN;
+    } else {
+        data_stats.average = std::accumulate(data.begin(), data.end(), 0.0) / data.size();
+        data_stats.min = *(std::min_element(data.begin(), data.end()));
+        data_stats.max = *(std::max_element(data.begin(), data.end()));
+    }
+    return data_stats;
 }
--- stats.h
+++ stats.h
@@ -1,5 +1,34 @@
 #include <vector>
+#include <bits/stdc++.h>
+
+class Stats
+{
+public:
+    float average;
+    float max;
+    float min;
+};
+
+class IAlerter {};
+
+class EmailAlert : public IAlerter
+{
+public:
+    bool emailSent;
+};
+class LEDAlert : public IAlerter
+{
+public:
+    bool ledGlows;
+};
+class StatsAlerter
+{
+public:
+    StatsAlerter(const float, const std::vector<IAlerter*>&);
+    void checkAndAlert(const std::vector<float>&);
+};
+
 
 namespace Statistics {
-    Stats ComputeStatistics(const std::vector<___>& );
+    Stats ComputeStatistics(const std::vector<float>&);
 }
